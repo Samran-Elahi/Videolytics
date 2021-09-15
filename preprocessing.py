9c7f1ce991274b183a60408d1def005987b6bc27
Samran-Elahi <44721060+Samran-Elahi@users.noreply.github.com>
2021-09-15 23:23:14 +0500

    minor changes
    - added doc string 
    - added tqdm 
    - changed class name

--- preprocessing.py
+++ preprocessing.py
@@ -9,9 +9,10 @@ import ffmpy3
 from ffmpy3 import FFmpeg
 import math
 import h5py
+from tqdm import tqdm
 
 
-class anomaly():
+class video_processing():
     def __init__(self):
         super().__init__()
     
@@ -24,25 +25,56 @@ class anomaly():
        except:
            print("error encountered")
     
-    def load_videos_from_dir(self, path):
+    def load_videos_from_dir(self, path, width = 240, height=240, frame_rate=2, segment=32,normalize = True):
+        """ This function is used to convert an entire directory of videos 
+        into numpy array at a fixed fps and fixed size bags 
+
+        Args: 
+            path (str) : path to the directory containing videos 
+            width (int) : width of the extracted frame(Default = 240)
+            height (int) : height of the extracted frame(Default = 240)
+            frame_rate (int) : this is the extracted fps (Deafault = 2)
+            segment (int) : The maximum capacity of a bag/ batch, note a batch/bag can be less then the max capacity but not greater then it (Default = 32)
+            normalize (Bool) : will convert the numpy frames into 0 to 1 range.
+
+        Returns: 
+             returns a 5 dimension numpy array which contains multiple bags/batches hold multiple video frames    
+
+        """
         # load all the videos in a directory 
-        vid_data = [] 
+        vid_data = []
+        segment = segment - 1 
         video_path = os.path.join(path, os.listdir(path)[0])
-        e1 = ext.Extractor(video_path, 60, 1)
-        data = e1.load_vid_data()
-        for i in range (1, 50):
+        e1 = ext.Extractor(video_path, segment, frame_rate)
+        data = e1.load_vid_data(width,height,normalize)
+        for i in tqdm (range (1, 50)):
             video_path = os.path.join(path, os.listdir(path)[i])
-            print("\nloading video number : ", i )
-            print("path = ", video_path)
-            e1 = ext.Extractor(video_path, 60, 1)
-            data2 = e1.load_vid_data()
-            print("shape of Data : ",data2.shape)
+            # print("\nloading video number : ", i )
+            # print("path = ", video_path)
+            e1 = ext.Extractor(video_path, segment, frame_rate)
+            data2 = e1.load_vid_data(width,height,normalize)
+            # print("shape of Data : ",data2.shape)
             data = np.concatenate((data,data2), axis=0)
         return data
 
     def load_video( self, path, width=240,height=240, frame_rate = 2, segment=32, normalize=True):
+        """ This function is used to convert an videos 
+        into numpy array at a fixed fps and fixed size bags 
+
+        Args: 
+            path (str) : path to the directory containing videos 
+            width (int) : width of the extracted frame(Default = 240)
+            height (int) : height of the extracted frame(Default = 240)
+            frame_rate (int) : this is the extracted fps (Deafault = 2)
+            segment (int) : The maximum capacity of a bag/ batch, note a batch/bag can be less then the max capacity but not greater then it (Default = 32)
+            normalize (Bool) : will convert the numpy frames into 0 to 1 range.
+
+        Returns: 
+             returns a 5 dimension numpy array which contains multiple bags/batches hold multiple video frames    
+
+        """
         segment = segment-1
-        e1 = ext.Extractor(video_path, segment, frame_rate)
+        e1 = ext.Extractor(path, segment, frame_rate)
         data = e1.load_vid_data(width,height,normalize)
         return data 
 
@@ -87,15 +119,15 @@ class anomaly():
         for i in range(len(data)):
             filename = "output" +str(i) +".webm"
             path = dir_path + filename
-            out = cv2.VideoWriter(path , cv2.VideoWriter_fourcc(*"vp80"), frame_rate, (320, 240))
+            out = cv2.VideoWriter(path , cv2.VideoWriter_fourcc(*"vp80"), frame_rate=1, dim = (320, 240))
             for frame in data[i]:
                 out.write(frame) # frame is a numpy.ndarray with shape (240, 320, 3)
             out.release()
 
-    def extract_feat_and_save_video(self , path , filename, frame_rate):
+    def extract_feat_and_save_video(self , path , filename, frame_rate=2,segment = 32):
 
        video_path = path
-       e1 = ext.Extractor(video_path, 60, 1)
+       e1 = ext.Extractor(video_path, segment, frame_rate)
         # saving the segmentated frames as a video in the static folder
        dir_path = "/home/khawar/Documents/AutoEncoder/Samran_Code/api/static/"
     
